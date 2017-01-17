32c01603a90a08459814852ba0c531c5b08091ec
Russell Yanofsky <russ@yanofsky.org>
2017-01-17 10:45:26 -0500

    fixup! [RPC] bumpfee
    Avoid replaces_txid / replaced_by_txid checks in CWallet::AvailableCoins when
    not requesting only confirmed transactions as suggested by @TheBlueMatt in
    https://github.com/bitcoin/bitcoin/pull/8456#discussion_r96332908

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1817,6 +1817,37 @@ bool CWalletTx::IsTrusted() const
     if (!InMempool())
         return false;
 
+    // We should not consider coins from transactions that are replacing
+    // other transactions.
+    //
+    // Example: There is a transaction A which is replaced by bumpfee
+    // transaction B. In this case, we want to prevent creation of
+    // a transaction B' which spends an output of B.
+    //
+    // Reason: If transaction A were initially confirmed, transactions B
+    // and B' would no longer be valid, so the user would have to create
+    // a new transaction C to replace B'. However, in the case of a
+    // one-block reorg, transactions B' and C might BOTH be accepted,
+    // when the user only wanted one of them. Specifically, there could
+    // be a 1-block reorg away from the chain where transactions A and C
+    // were accepted to another chain where B, B', and C were all
+    // accepted.
+    if (mapValue.count("replaces_txid")) {
+        return false;
+    }
+
+    // Similarly, we should not consider coins from transactions that
+    // have been replaced. In the example above, we would want to prevent
+    // creation of a transaction A' spending an output of A, because if
+    // transaction B were initially confirmed, conflicting with A and
+    // A', we wouldn't want to the user to create a transaction D
+    // intending to replace A', but potentially resulting in a scenario
+    // where A, A', and D could all be accepted (instead of just B and
+    // D, or just A and A' like the user would want).
+    if (mapValue.count("replaced_by_txid")) {
+        return false;
+    }
+
     // Trusted if all inputs are from us and are in the mempool:
     BOOST_FOREACH(const CTxIn& txin, tx->vin)
     {
@@ -2016,37 +2047,6 @@ void CWallet::AvailableCoins(vector<COutput>& vCoins, bool fOnlyConfirmed, const
             // It's possible for these to be conflicted via ancestors which we may never be able to detect
             if (nDepth == 0 && !pcoin->InMempool())
                 continue;
-
-            // We should not consider coins from transactions that are replacing
-            // other transactions.
-            //
-            // Example: There is a transaction A which is replaced by bumpfee
-            // transaction B. In this case, we want to prevent creation of
-            // a transaction B' which spends an output of B.
-            //
-            // Reason: If transaction A were initially confirmed, transactions B
-            // and B' would no longer be valid, so the user would have to create
-            // a new transaction C to replace B'. However, in the case of a
-            // one-block reorg, transactions B' and C might BOTH be accepted,
-            // when the user only wanted one of them. Specifically, there could
-            // be a 1-block reorg away from the chain where transactions A and C
-            // were accepted to another chain where B, B', and C were all
-            // accepted.
-            if (nDepth == 0 && pcoin->mapValue.count("replaces_txid")) {
-                continue;
-            }
-
-            // Similarly, we should not consider coins from transactions that
-            // have been replaced. In the example above, we would want to prevent
-            // creation of a transaction A' spending an output of A, because if
-            // transaction B were initially confirmed, conflicting with A and
-            // A', we wouldn't want to the user to create a transaction D
-            // intending to replace A', but potentially resulting in a scenario
-            // where A, A', and D could all be accepted (instead of just B and
-            // D, or just A and A' like the user would want).
-            if (nDepth == 0 && pcoin->mapValue.count("replaced_by_txid")) {
-                continue;
-            }
 
             for (unsigned int i = 0; i < pcoin->tx->vout.size(); i++) {
                 isminetype mine = IsMine(pcoin->tx->vout[i]);
